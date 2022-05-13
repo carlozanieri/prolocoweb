3ca20de38bfaa5fefe77d12510f3ee68e8efadd8
carlozanieri <carlo.zanieri@gmail.com>
2022-05-13 20:12:09 +0200

    Versione 1.2

--- hello.py
+++ hello.py
@@ -28,17 +28,8 @@ class HelloWorld():
            tmpl = env.get_template('mytemplate.html')
            page = tmpl.render(pag=pag,blogs=Connect.blog(""), target='World',  manifestazione="blog", menu=Connect.menu(""), submenu=Connect.submnu(""),pagina=Connect.body("", "mugello"),  luogo = "mugello", urlx=cherrypy.url())
         
-        elif cherrypy.url() == 'http://localhost/?pag=blog' :
-            tmpl = env.get_template('mytemplate.html')
-            par="blog"
-            page = tmpl.render(pag="blog",blogs=Connect.blog(""), target='World',  manifestazione="blog", menu=Connect.menu(""), submenu=Connect.submnu(""),pagina=Connect.body("", "mugello"),  luogo = "mugello", urlx=cherrypy.url())
         
-        elif cherrypy.url() == 'http://carlozanieri.it/?pag=blog' :
-            tmpl = env.get_template('mytemplate.html')
-            par="blog"
-            page = tmpl.render(pag="blog",blogs=Connect.blog(""), target='World',  manifestazione="blog", menu=Connect.menu(""), submenu=Connect.submnu(""),pagina=Connect.body("", "mugello"),  luogo = "mugello", urlx=cherrypy.url())
-      
-       
+            
         elif cherrypy.url() == 'http://0.0.0.0/' :
             tmpl = env.get_template('index.html')
             page = tmpl.render(blogs=Connect.blog(""), target='World',  menu=Connect.menu(""), submenu=Connect.submnu(""),pagina=Connect.body("", "mugello"),  luogo = "mugello", urlx=cherrypy.url())
